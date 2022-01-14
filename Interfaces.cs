53ff79f503b81108e6824e27d180154f61378c14
Ilya Kunitsky <ilyhhaa@gmail.com>
2022-01-15 01:05:37 +0300

    Add files via upload

---
+++ Interfaces.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Interfaces
+{
+    public interface IAcolyte
+    {
+        const string Name = "Прислужник";
+        const int Health = 220;
+        const int Damage = 10;
+        void Phrase();
+        
+        
+    }
+    public interface IPeasant
+    {
+        const string Name = "Рабочий";
+        const int Health = 220;
+        const int Damage = 6;
+        void Phrase();
+        
+    }
+    public interface IPeon
+    {
+        const string Name = "Раб";
+        const int Health = 250;
+        const int Damage = 8;
+        void Phrase();
+        
+
+    }
+    public interface IWisp
+    {
+        const string Name = "Светлячок";
+        const int Health = 120;
+        const int Damage = 0;
+        void Phrase();
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Interfaces
+{
+    abstract class Unit
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public void Display() => Console.WriteLine($"Здоровье юнита = {Health} , урон юнита = {Damage}");
+        public void DisplayName() => Console.WriteLine(Name);
+    }
+    class Acolyte:Unit,IAcolyte
+    {
+        
+        public Acolyte()
+        {
+            
+            this.Name = IAcolyte.Name;
+            DisplayName();
+            this.Health = IAcolyte.Health;
+            this.Damage = IAcolyte.Damage;
+            Display();
+            Phrase();
+
+        }
+        public void Phrase() => Console.WriteLine("Нужно больше золота");
+    }
+
+    class Peon:Unit,IPeon 
+    {
+        
+        public Peon()
+        {
+            
+            this.Name = IPeon.Name;
+            DisplayName();
+            this.Health = IPeon.Health;
+            this.Damage = IPeon.Damage;
+            Display();
+            Phrase();
+
+        }
+        public void Phrase() => Console.WriteLine("Не могу стоять, пока другие работают... Пойду полежу.");
+
+
+    }
+    class Peasant : Unit, IPeasant
+    {
+
+        public Peasant()
+        {
+            
+            this.Name = IPeasant.Name;
+            DisplayName();
+            this.Health = IPeasant.Health;
+            this.Damage = IPeasant.Damage;
+            Display();
+            Phrase();
+
+        }
+        public void Phrase() => Console.WriteLine("Опять работа?");
+
+
+    }
+    class Wisp : Unit, IWisp
+    {
+
+        public Wisp()
+        {
+            
+            this.Name = IWisp.Name;
+            DisplayName();
+            this.Health = IWisp.Health;
+            this.Damage = IWisp.Damage;
+            Display();
+            Phrase();
+
+        }
+        public void Phrase() => Console.WriteLine(".оО");
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Acolyte acolyte = new Acolyte();
+            Peon peon = new Peon();
+            Peasant peasant = new Peasant();
+            Wisp wisp = new Wisp();
+
+
+
+
+        }
+    }
+}
+    
+   
+
